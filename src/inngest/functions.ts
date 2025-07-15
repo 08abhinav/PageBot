bcceaf7a69156b7d4f063f1be1075ee53bd349e0
08abhinav <abhinavnegi101@gmail.com>
2025-07-15 12:33:33 +0530

    updated: added new function geminiAgent

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,19 @@
+import { gemini, createAgent } from "@inngest/agent-kit";
 import {inngest} from "./client"
 
-export const helloWorld = inngest.createFunction(
-    {id: "hello - world"},
-    {event: "test/hello.world"},
-    async ({event, step}) =>{
-        await step.sleep("wati a moment", "1s");
-        return {message: `Hello ${event.data.email}`};
+export const geminiAgent = inngest.createFunction(
+    {id: "gemini-agent"},
+    {event: "test/gemini.agent"},
+    async ({event})=>{
+        const summarizer = createAgent({
+            name: "summarizer",
+            system: "You are one of the talented investor. You tell people where to invest, why to invest, what are the risk factors, how much will they get in return. You have a talent that you only give a profitable advices.",
+            model: gemini({ model: "gemini-1.5-flash" })
+        })
+
+        const {output} = await summarizer.run(
+            `Summarize the following test: ${event.data.value}`
+        )
+        return {output}
     }
 )
