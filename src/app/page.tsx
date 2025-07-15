ab7a40fd17b30a0f39737b0b6db232ec9d565951
08abhinav <abhinavnegi101@gmail.com>
2025-07-15 12:35:24 +0530

    update: added input and button for testing background jobs and gemini model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,24 @@
 "use client"
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
 
+import { useTRPC } from "@/trpc/client"
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+   
 const Page = () =>{
+  const [value, SetValue] = useState("")
   const trpc = useTRPC();
-  const {data} = useQuery(trpc.hello.queryOptions({text :"hello"})
-)
+  const invoke = useMutation(trpc.invoke.mutationOptions({}))
   return(
-    <div>
-      {JSON.stringify(data)}
-    </div>
-  )
+    <div className="p-4 max-w-7xl mx-auto">
+      
+      <Input type="text"
+      value={value}
+      onChange={(e)=>SetValue(e.target.value)} />
+      <button onClick={() => invoke.mutate({value: value})}>
+        Invoke Background Job
+      </button> 
+    </div>)
 }
 
-export default Page
+export default Page  
